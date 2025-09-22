084fbf7a68ce709dc7df464fc6d4bba2f509a9e6
sanjeev bhawra <bhawsanjeev102@gmail.com>
2025-09-22 20:25:37 +0530

    modify config ...cantrollers .... routes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use("/api/internships", internshipRoutes);
 
-app.get("/", (req, res) => res.send("Backend is running..."));
+app.get("/", (req, res) => res.send(" heyy sanjeev`s backend is running..."));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
